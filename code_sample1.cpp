df2874fccbbd72e11f2c0778b6121ae832000111
xuezhou1998 <38165217+xuezhou1998@users.noreply.github.com>
2021-09-30 01:51:35 -0400

    this is my code sample

---
+++ code_sample1.cpp
@@ -0,0 +1,81 @@
+vector<int> resInstr(string addrMod, int op, int modNumb){
+  int result=0;
+  int opcode=op/1000;
+  int operand=op % 1000;
+  int err=-1;
+  int baseAddr=modules[modNumb];
+  int unDef=-1;
+
+   if (addrMod=="I"){
+    if (op<10000){
+      result=op;
+    }
+    else{
+      err=4;
+      result=9999;
+    }
+
+  }
+  else if (addrMod=="E"){
+    int newOperand=0;
+
+    if (operand<usecount){
+
+      result=opcode*1000;
+      if (symbolTab.count(symLst[operand])<=0){
+        result+=0;
+        err=6;
+        unDef=operand;
+        std::vector<string>::iterator position =
+        std::find(used_Lst.begin(), used_Lst.end(), symLst[operand]);
+        if (position != used_Lst.end()){
+          used_Lst.erase(position);
+        }
+      }
+      else{
+        newOperand=symbolTab[symLst[operand]];
+
+        result+=newOperand;
+        std::vector<string>::iterator position = std::find(used_Lst.begin(), used_Lst.end(), symLst[operand]);
+        if (position != used_Lst.end()){
+          used_Lst.erase(position);
+        }
+
+        symbolErr[symLst[operand]]-=1;
+      }
+    }
+    else {
+      err=2;
+      result=op;
+    }
+  }
+  else if (addrMod=="R"){
+    if (modulesLen[modNumb]>operand){
+      result=opcode*1000;
+      result+=operand+baseAddr;
+    }
+    else{
+      result=opcode*1000;
+      result+=baseAddr;
+      err=1;
+    }
+
+  }
+  else if (addrMod=="A"){
+    if (operand<512){
+      result=opcode*1000;
+      result+=operand;
+    }
+    else if (operand>=512){
+      err=0;
+      result=opcode*1000;
+      result+=0;
+    }
+  }
+  if (opcode>=10 && err==-1){
+      err=5;
+      result=9999;
+    }
+  vector<int> resV={result,err,operand,opcode,unDef};
+  return resV;
+}
